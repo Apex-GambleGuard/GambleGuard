65af1e42823a226ddb1403d8c57f65ee4d765643
Apex-GambleGuard <nilscuber9@gmail.com>
2026-08-03 20:23:29 +0200

    Add website detection

--- extension/src/background.js
+++ extension/src/background.js
@@ -1,3 +1,17 @@
 chrome.runtime.onInstalled.addListener(() => {
     console.log("✅ GambleGuard installed");
 });
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+
+    if (changeInfo.status !== "complete") {
+        return;
+    }
+
+    if (!tab.url) {
+        return;
+    }
+
+    console.log("🌍 Besuchte Webseite:", tab.url);
+
+});
